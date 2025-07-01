49db2a4a9451a8fe4a9bb307476e6a45dd48e8f4
Diego Ruiz <167106667+druizclids@users.noreply.github.com>
2025-06-30 19:34:33 -0500

    Backend
    Se agrega backend actualizado para la prueba técnica Godoy Cordoba

---
+++ Back-end/Prueba_Api/Prueba_Api/Controllers/CatFactController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prueba_Api.Models;
+using Prueba_Api.Repositories;
+using Prueba_Api.Services;
+
+namespace Prueba_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatFactController
+    {
+        private readonly ICatFactService _service;
+
+        public CatFactController(ICatFactService service, IGifRepository gifRepository)
+        {
+            _service = service;
+        }
+
+        [HttpGet("random")]
+        public async Task<ActionResult<CastFact>> GetRandomFact()
+        {
+            var response = await _service.GetFactWithWordsAsync();
+            return response;
+        }
+
+        [HttpGet("gif/{id}")]
+        public async Task<ActionResult<CastFact>> ReprocessGif(int id)
+        {
+            var response = await _service.ReprocessGifAsync(id);
+
+            if (response == null)
+                return null;
+
+            return response;
+        }
+
+        [HttpPost("history")]
+        public async Task<ActionResult<List<CastFact>>> GetHistory()
+        {
+            var history = await _service.GetHistoryAsync();
+            return history;
+        }
+
+
+
+    }
+}
---
+++ Back-end/Prueba_Api/Prueba_Api/Entities/CatFactEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba_Api.Entities
+{
+    public class CatFactEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Fact { get; set; }
+        public string Palabra1 { get; set; }
+        public string Palabra2 { get; set; }
+        public string Palabra3 { get; set; }
+        public string GifUrl { get; set; }
+        public DateTime FechaBusqueda { get; set; }
+    }
+}
